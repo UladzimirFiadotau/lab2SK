64622932b5890c3632d8ea4433b14b268c5349c0
Admin <admin@Mac-Admin.local>
2015-09-18 21:14:52 +0300

    Initial Commit

---
+++ AppleTreeMobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleTreeMobile
+//
+//  Created by Admin on 18.09.15.
+//  Copyright (c) 2015 Uladzimir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
